28ac40f424fcae8586483ee7a8edd43c3f5274e4
leonardomontilla74 <leme.310589@gmail.com>
2022-02-27 16:51:02 -0400

    Llamado a URL con API_KEY

--- api/src/preload.js
+++ api/src/preload.js
@@ -1,14 +1,15 @@
 const axios = require('axios');
 const { Temperament } = require('./db');
 const URL = 'https://api.thedogapi.com/v1/breeds';
+const { API_KEY } = process.env;
 
 module.exports = async function preLoader() {
     try {
         let temperaments = [];
 
-        const allDogs = await axios(URL);
+        const allDogs = await axios(URL + API_KEY);
         allDogs.data?.map((dog) => {
-            if (dog.temperament) { // no todos los perros tienen temperamento?
+            if (dog.temperament) { // no todos los perros tienen temperamento
                 const arrayOfTemps = dog.temperament.split(', '); // => ['Stubborn', 'Curious', 'Playful', 'Adventurous', 'Active', 'Fun-loving']
                 temperaments = [...temperaments, ...arrayOfTemps]; // en cada vuelta guardo lo que ya tenia y lo concateno con el nuevo dog
             }
