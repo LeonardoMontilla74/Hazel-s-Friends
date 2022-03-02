956bf979bb369442813e06cdcecd20b23d4aac6c
leonardomontilla74 <leme.310589@gmail.com>
2022-03-02 03:00:33 -0400

    Creando las primeras rutas

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import { Route } from "react-router-dom";
+import Landing from "./React/Landing";
+import Home from './React/Home';
+import Temperaments from "./React/Temperaments";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Dogs</h1>
+    <div>
+      <Route exact path='/' component={Landing} />
+      <Route path='/dogs' component={Home} />
+      <Route path='/temperaments' component={Temperaments} />
     </div>
   );
 }
