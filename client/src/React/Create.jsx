5cae8b47516d004dae7e30eaaa07b48277fa237c
leonardomontilla74 <leme.310589@gmail.com>
2022-03-02 03:01:28 -0400

    Componentes primer paso

---
+++ client/src/React/Create.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Create() {
+    return (
+        <div>Estamos en /Createdog</div>
+    );
+}
---
+++ client/src/React/Temperaments.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Temperaments() {
+    return (
+        <div>Hola estamos en temperaments</div>
+    );
+}
