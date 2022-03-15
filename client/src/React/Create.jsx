a64d38471c052083fe6f26e0e2bd5e609b59b439
leonardomontilla74 <leme.310589@gmail.com>
2022-03-15 03:27:48 -0400

    Form create listo

--- client/src/React/Create.jsx
+++ client/src/React/Create.jsx
@@ -1,84 +1,162 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getTemp, createDog } from "../Redux/actions";
+import { useHistory } from 'react-router-dom'
+import styles from '../Styles/Create.module.css'
 
 export default function Create() {
 
     const dispatch = useDispatch();
     const temperaments = useSelector((state) => state.temperaments);
+    const history = useHistory()
 
     useEffect(() => {
-
         dispatch(getTemp());
-
     }, [dispatch]);
 
-
-    const [input, setInput] = useState({
-        name: '',
-        height_min: '',
-        height_max: '',
-        weight_min: '',
-        weight_max: '',
-        life: '',
-        origin: '',
-        bred_for: '',
+    const [inputs, setInputs] = useState({
         temperaments: [],
     });
 
+    const [errors, setErrors] = useState({
+        name: '',
+        height_max: '',
+        weight_max: '',
+        origin: '',
+        bred_for: ''
+    });
+
+    function validate(inputs) {
+
+        let error = {};
+        if (/\d+/.test(inputs.name)) error.name = "El nombre es inválido";
+        if (/\d+/.test(inputs.bred_for)) error.bred_for = "Debe ser un texto";
+        if (/\d+/.test(inputs.origin)) error.origin = "Debe ser un texto";
+        if (inputs.height_max < inputs.height_min) error.height_max = 'La altura máxima debe ser mayor';
+        if (inputs.weight_max < inputs.weight_min) error.weight_max = 'El peso máximo debe ser mayor';
+
+        setErrors(error);
+    }
+
     const handleChange = (e) => {
         if (e.target.name === 'temperaments') {
-            setInput({
-                ...input,
-                temperaments: [...input.temperaments, Number(e.target.value)],
+            setInputs({
+                ...inputs,
+                temperaments: [...inputs.temperaments, Number(e.target.value)],
             });
         } else if (e.target.type === 'number') {
-            setInput({
-                ...input,
+            setInputs({
+                ...inputs,
                 [e.target.name]: Number(e.target.value)
             });
         }
         else {
-            setInput({
-                ...input,
+            setInputs({
+                ...inputs,
                 [e.target.name]: e.target.value
             });
         }
+        validate(inputs)
     };
 
     const onSubmit = (input) => {
         dispatch(createDog(input));
-        console.log(input);
-        setInput({});
+        alert("Creación exitosa");
+        history.push('/dogs');
     };
 
     return (
         <div>
             <form>
                 <label>Nombre:
-                    <input type="text" name="name" value={input.name} onChange={handleChange} />
+                    <input
+                        type="text"
+                        placeholder="Ej. Hazel..."
+                        name="name"
+                        value={inputs.name}
+                        autoComplete="off"
+                        className={errors.name ? styles.error : styles.ok}
+                        onChange={handleChange} />
+                    {errors.name ? <p className={styles.error}>{errors.name}</p> : null}
                 </label>
-                <label>Altura minima:
-                    <input type="number" name="height_min" value={input.height_min} onChange={handleChange} />
-                </label>
+                <br />
+
                 <label>Altura máxima:
-                    <input type="number" name="height_max" value={input.height_max} onChange={handleChange} />
+                    <input
+                        type="number"
+                        placeholder="Ej. 80... Debe ser mayor"
+                        name="height_max"
+                        value={inputs.height_max}
+                        className={errors.height_max ? styles.error : styles.ok}
+                        onChange={handleChange} />
+                    {errors.height_max ? <p className={styles.error} >{errors.height_max}</p> : null}
                 </label>
-                <label>Peso minimo:
-                    <input type="number" name="weight_min" value={input.weight_min} onChange={handleChange} />
+
+                <label>Altura minima:
+                    <input
+                        type="number"
+                        placeholder="Ej. 60..."
+                        name="height_min"
+                        value={inputs.height_min}
+                        onChange={handleChange} />
                 </label>
+                <br />
+
                 <label>Peso máximo
-                    <input type="number" name="weight_max" value={input.weight_max} onChange={handleChange} />
+                    <input
+                        type="number"
+                        placeholder="Ej. 8... Debe ser mayor"
+                        name="weight_max"
+                        value={inputs.weight_max}
+                        className={errors.weight_max ? styles.error : styles.ok}
+                        onChange={handleChange} />
+                    {errors.weight_max ? <p className={styles.error}>{errors.weight_max}</p> : null}
                 </label>
+
+                <label>Peso minimo:
+                    <input
+                        type="number"
+                        placeholder="Ej. 4..."
+                        name="weight_min"
+                        value={inputs.weight_min}
+                        onChange={handleChange} />
+                </label>
+                <br />
+
                 <label>Edad:
-                    <input type="number" name="life" value={input.life} onChange={handleChange} />
+                    <input
+                        type="number"
+                        placeholder="Ej. 3.."
+                        name="life"
+                        value={inputs.life}
+                        onChange={handleChange} />
                 </label>
+                <br />
+
                 <label>Origen:
-                    <input type="text" name="origin" value={input.origin} onChange={handleChange} />
+                    <input
+                        type="text"
+                        placeholder="Ej. Venezuela..."
+                        name="origin"
+                        value={inputs.origin}
+                        autoComplete='off'
+                        className={errors.origin ? styles.error : styles.ok}
+                        onChange={handleChange} />
                 </label>
+                <br />
+
                 <label>Ideal para:
-                    <input type="text" name="bred_for" value={input.bred_for} onChange={handleChange} />
+                    <input
+                        type="text"
+                        placeholder="Ej. Correr..."
+                        name="bred_for"
+                        value={inputs.bred_for}
+                        autoComplete='off'
+                        className={errors.bred_for ? styles.error : styles.ok}
+                        onChange={handleChange} />
                 </label>
+                <br />
+
                 <label>Temperamentos:
                     <select name="temperaments" onChange={handleChange} >
                         <option value="">Seleciona un temperamento</option>
@@ -88,10 +166,16 @@ export default function Create() {
                             );
                         })}
                     </select>
+                    <p>Elgiste los temperamentos # {inputs.temperaments?.join(' - ')}</p>
                 </label>
-                <p>{input.temperaments.join(' - ')}</p>
+                <br />
+
             </form>
-            <button onClick={() => onSubmit(input)}>Crear perrito</button>
+
+            <button
+                className={styles.buttonGet}
+                onClick={() => onSubmit(inputs)}>Crear perrito</button>
+
         </div>
     );
 }
