6f323fd68fa904cc316bed2fbf515a0617a6d62e
Leonardo Montilla <leme.310589@gmail.com>
2022-07-07 07:57:06 -0400

    fixed (package) add react-scripts

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import dotenv from 'dotenv';
 import axios from 'axios';
 import store from './Redux/store';
 import App from './App';
 import './index.css';
 
-dotenv.config();
-axios.defaults.baseURL = REACT_APP_URL || "http://localhost:3001";
+axios.defaults.baseURL = `https://hazel-friends.herokuapp.com`
 
 ReactDOM.render(
   <Provider store={store}>
